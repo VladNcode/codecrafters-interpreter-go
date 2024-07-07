ee740d1077c36f713705ad8f2967ac30a4ff1076
vladncode <vladncode@gmail.com>
2024-07-07 17:01:59 +0300

    codecrafters submit [skip ci]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 	"strings"
 )
 
+func isAlpha(c string) bool {
+	return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") || c == "_"
+}
+
+func isDigit(c string) bool {
+	return c >= "0" && c <= "9"
+}
+
+func isStringStart(c string) bool {
+	return c == "\""
+}
+
 func isIdentifier(c string) bool {
-	return (c >= "a" && c <= "z") ||
-		(c >= "A" && c <= "Z") || (c >= "0" && c <= "9") || c == "_"
+	return isDigit(c) || isAlpha(c)
 }
 
 func removeTrailingZeroes(s string) string {
@@ -17,35 +27,11 @@ func removeTrailingZeroes(s string) string {
 		return s
 	}
 
-	// Remove all trailing zeroes
-	s = strings.TrimRight(s, "0")
-
-	// Add a single trailing zero
-	return s + "0"
+	return strings.TrimRight(s, "0") + "0"
 }
 
-func main() {
+func tokenize(rawFileContents []byte) {
 	exitCode := 0 // Default exit code
-
-	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
-		os.Exit(1)
-	}
-
-	command := os.Args[1]
-
-	if command != "tokenize" {
-		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
-		os.Exit(1)
-	}
-
-	filename := os.Args[2]
-	rawFileContents, err := os.ReadFile(filename)
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-		os.Exit(1)
-	}
 
 	tokenToType := map[string]string{
 		"(":  "LEFT_PAREN",
@@ -109,167 +95,101 @@ func main() {
 
 	lines := strings.Split(string(rawFileContents), "\n")
 
-	isDigit := regexp.MustCompile(`[0-9]`)
-
 	for lineNumber, line := range lines {
 		shouldSkip := false
 
-		identifierMode := false
-		identifierText := ""
-
-		stringMode := false
-		stringText := ""
-
-		numberMode := false
-		numberText := ""
-		float := false
-
-		for idx := 0; idx < len(line); idx++ {
+		for idx := 0; idx < len(line); {
 			token := string(line[idx])
 
-			if tokenType, ok := tokenToType[token]; !ok && token != "\"" && !identifierMode && !stringMode && !numberMode && !isDigit.MatchString(token) && !isIdentifier(token) {
-				fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %s\n", lineNumber+1, token)
-				exitCode = 65
-			} else if idx == len(line)-1 && stringMode && token != "\"" {
-				fmt.Fprintf(os.Stderr, "[line %d] Error: Unterminated string.\n", lineNumber+1)
-				exitCode = 65
-			} else {
+			if isStringStart(token) {
+				stringText := token
+				idx += 1
 
-				if numberMode && !isDigit.MatchString(token) && token != "." {
-					if float {
-						fmt.Printf("NUMBER %s %s\n", numberText, removeTrailingZeroes(numberText))
-					} else {
-						fmt.Printf("NUMBER %s %s.0\n", numberText, numberText)
-					}
-
-					numberMode = false
-					numberText = ""
-					float = false
-				}
-
-				if isIdentifier(token) && !stringMode && !numberMode {
-					if isDigit.MatchString(token) {
-						if identifierMode {
-							identifierText += token
-
-							if idx != len(line)-1 {
-								continue
-							}
-						}
-					} else {
-						if !identifierMode {
-							identifierMode = true
-						}
-
-						identifierText += token
-
-						if idx != len(line)-1 {
-							continue
-						}
-					}
-
-				}
-
-				if (!isIdentifier(token) || idx == len(line)-1) && identifierMode {
-
-					reservedWord := reservedWords[identifierText]
-
-					if reservedWord != "" {
-						fmt.Printf("%s %s null\n", reservedWord, identifierText)
-					} else {
-						fmt.Printf("IDENTIFIER %s null\n", identifierText)
-					}
-
-					identifierMode = false
-					identifierText = ""
-				}
-
-				if token == "\"" {
-					if !stringMode {
-						stringMode = true
-						continue
-					}
-
-					fmt.Printf("STRING \"%s\" %s\n", stringText, stringText)
-					stringText = ""
-					stringMode = false
-					continue
-				}
-
-				if stringMode {
+				for idx < len(line) {
+					token = string(line[idx])
 					stringText += token
-					continue
-				}
 
-				if isDigit.MatchString(token) {
-					if !numberMode {
-						numberMode = true
-					}
+					idx += 1
 
-					numberText += token
-
-					if idx != len(line)-1 {
-						continue
-					}
-				}
-
-				if numberMode {
-					if token == "." {
-						if float {
-							fmt.Printf("NUMBER %s %s\n", numberText, numberText)
-							numberMode = false
-							numberText = ""
-							float = false
-							fmt.Printf("%s %s null\n", tokenType, token)
-						} else {
-							if idx != len(line)-1 {
-								float = true
-								numberText += token
-							} else {
-								fmt.Printf("NUMBER %s %s.0\n", numberText, numberText)
-								fmt.Printf("%s %s null\n", tokenType, token)
-							}
-						}
-
-						continue
-					} else {
-						if float {
-							fmt.Printf("NUMBER %s %s\n", numberText, removeTrailingZeroes(numberText))
-						} else {
-							fmt.Printf("NUMBER %s %s.0\n", numberText, numberText)
-						}
-
-						numberMode = false
-						numberText = ""
-						float = false
-
-						if isDigit.MatchString(token) {
-							continue
-						}
-					}
-
-				}
-
-				if whiteSpace[token] {
-					continue
-				}
-
-				if operators[token] && idx+2 <= len(line) && doublesToType[line[idx:idx+2]] != "" {
-					token = line[idx : idx+2]
-					tokenType = doublesToType[token]
-
-					if tokenType == "Comment" {
-						shouldSkip = true
+					if token == "\"" {
 						break
 					}
+				}
 
+				if !strings.HasSuffix(stringText, "\"") {
+					fmt.Fprintf(os.Stderr, "[line %d] Error: Unterminated string.\n", lineNumber+1)
+					exitCode = 65
+					continue
+				}
+
+				fmt.Printf("STRING %s %s\n", stringText, strings.Trim(stringText, "\""))
+
+				continue
+			} else if isDigit(token) {
+				periodCounter := int(0)
+				numberText := ""
+
+				for idx < len(line) && (isDigit(string(line[idx])) || string(line[idx]) == ".") && periodCounter <= 1 {
+					if string(line[idx]) == "." {
+						if periodCounter == int(1) || idx == len(line)-1 {
+							break
+						}
+
+						periodCounter = 1
+					}
+
+					numberText += string(line[idx])
 					idx += 1
 				}
 
-				if tokenType != "" && token != "" {
-					fmt.Printf("%s %s null\n", tokenType, token)
+				if periodCounter > 0 {
+					fmt.Printf("NUMBER %s %s\n", numberText, removeTrailingZeroes(numberText))
+				} else {
+					fmt.Printf("NUMBER %s %s.0\n", numberText, numberText)
 				}
 
+				continue
+			} else if isAlpha(token) {
+				identefierText := ""
+
+				for idx < len(line) && isIdentifier(string(line[idx])) {
+					identefierText += string(line[idx])
+					idx += 1
+				}
+
+				reservedWord := reservedWords[identefierText]
+
+				if reservedWord != "" {
+					fmt.Printf("%s %s null\n", reservedWord, identefierText)
+				} else {
+					fmt.Printf("IDENTIFIER %s null\n", identefierText)
+				}
+
+				continue
+			} else if whiteSpace[token] {
+				idx++
+				continue
+			} else if operators[token] && idx+2 <= len(line) && doublesToType[line[idx:idx+2]] != "" {
+				token = line[idx : idx+2]
+				tokenType := doublesToType[token]
+
+				if tokenType == "Comment" {
+					shouldSkip = true
+					break
+				}
+
+				fmt.Printf("%s %s null\n", tokenType, token)
+				idx += 2
+				continue
+			} else if tokenToType[token] != "" && token != "" {
+				fmt.Printf("%s %s null\n", tokenToType[token], token)
+				idx++
+				continue
+			} else {
+				fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %s\n", lineNumber+1, token)
+				exitCode = 65
+				idx++
+				continue
 			}
 		}
 
@@ -280,4 +200,28 @@ func main() {
 
 	fmt.Printf("EOF  null")
 	os.Exit(exitCode)
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	if command != "tokenize" {
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+
+	filename := os.Args[2]
+	rawFileContents, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	tokenize(rawFileContents)
 }
