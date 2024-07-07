bc324d46420eec996e080ab3e4b9ee30c1303c13
vladncode <vladncode@gmail.com>
2024-07-07 10:08:02 +0300

    codecrafters submit [skip ci]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -30,21 +30,23 @@ func main() {
 	}
 
 	tokenToType := map[string]string{
-		"(": "LEFT_PAREN",
-		")": "RIGHT_PAREN",
-		"{": "LEFT_BRACE",
-		"}": "RIGHT_BRACE",
-		",": "COMMA",
-		".": "DOT",
-		"-": "MINUS",
-		"+": "PLUS",
-		";": "SEMICOLON",
-		"*": "STAR",
-		"/": "SLASH",
-		"=": "EQUAL",
-		"!": "BANG",
-		"<": "LESS",
-		">": "GREATER",
+		"(":  "LEFT_PAREN",
+		")":  "RIGHT_PAREN",
+		"{":  "LEFT_BRACE",
+		"}":  "RIGHT_BRACE",
+		",":  "COMMA",
+		".":  "DOT",
+		"-":  "MINUS",
+		"+":  "PLUS",
+		";":  "SEMICOLON",
+		"*":  "STAR",
+		"/":  "SLASH",
+		"=":  "EQUAL",
+		"!":  "BANG",
+		"<":  "LESS",
+		">":  "GREATER",
+		" ":  "SPACE",
+		"\t": "TAB",
 	}
 
 	doublesToType := map[string]string{
@@ -53,6 +55,11 @@ func main() {
 		"<=": "LESS_EQUAL",
 		">=": "GREATER_EQUAL",
 		"//": "Comment",
+	}
+
+	whiteSpace := map[string]bool{
+		" ":  true,
+		"\t": true,
 	}
 
 	operators := map[string]bool{
@@ -75,6 +82,10 @@ func main() {
 				fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %s\n", lineNumber+1, token)
 				exitCode = 65
 			} else {
+				if whiteSpace[token] {
+					continue
+				}
+
 				if operators[token] && idx+2 <= len(line) && doublesToType[line[idx:idx+2]] != "" {
 					token = line[idx : idx+2]
 					tokenType = doublesToType[token]
